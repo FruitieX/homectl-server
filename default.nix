4edb5dc61c1d1188bb48d90a3919aa0f8eb913c0
Rasmus Eskola <fruitiex@gmail.com>
2021-01-06 19:35:46 +0200

    Add rust extensions back to default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,21 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/8c007b60731c07dd7a052cce508de3bb1ae849b4.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  rustNightlyChannel = (nixpkgs.rustChannelOf { date = "2021-01-06"; channel = "nightly"; }).rust.override {
+    extensions = [
+      "rust-src"
+      "rust-analysis"
+      "rustfmt-preview"
+      "clippy-preview"
+    ];
+  };
 in
   with nixpkgs;
   stdenv.mkDerivation {
     name = "env";
     buildInputs = [
       # (nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
-      (nixpkgs.rustChannelOf { date = "2021-01-06"; channel = "nightly"; }).rust
+      rustNightlyChannel
 
       docker-compose
       openssl
