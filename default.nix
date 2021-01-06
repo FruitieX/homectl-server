55f5eedd07a082d2edc4ae767e571aa0ca26c01e
Rasmus Eskola <fruitiex@gmail.com>
2021-01-06 19:15:59 +0200

    Pin rust nightly version

--- default.nix
+++ default.nix
@@ -1,14 +1,15 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/8c007b60731c07dd7a052cce508de3bb1ae849b4.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rust-moz-overlay = (nixpkgs.latest.rustChannels.nightly.rust.override { extensions = [ "rust-src" "rust-analysis" "rustfmt-preview" ];});
 in
   with nixpkgs;
   stdenv.mkDerivation {
     name = "env";
     buildInputs = [
+      # (nixpkgs.rustChannelOf { rustToolchain = ./rust-toolchain; }).rust
+      (nixpkgs.rustChannelOf { date = "2021-01-06"; channel = "nightly"; }).rust
+
       docker-compose
-      rust-moz-overlay
       openssl
       pkg-config
     ];
