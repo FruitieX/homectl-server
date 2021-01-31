1ad303c8670f8270905b22e27d8bf45636503779
Rasmus Eskola <fruitiex@gmail.com>
2021-01-31 17:27:09 +0200

    Update rustNightlyChannel

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/8c007b60731c07dd7a052cce508de3bb1ae849b4.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rustNightlyChannel = (nixpkgs.rustChannelOf { date = "2021-01-06"; channel = "nightly"; }).rust.override {
+  rustNightlyChannel = (nixpkgs.rustChannelOf { date = "2021-01-16"; channel = "nightly"; }).rust.override {
     extensions = [
       "rust-src"
       "rust-analysis"
