a1e0a3d94817c2bf41ca7faf103cd06c84db7aa4
Rasmus Eskola <fruitiex@gmail.com>
2021-02-07 12:49:55 +0200

    Listen on 0.0.0.0 by default

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -7,13 +7,50 @@ mod devices;
 
 use devices::*;
 
-pub fn init_api(state: &Arc<AppState>) {
+use anyhow::Result;
+use rocket::config::{Config, Environment, LoggingLevel};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response,
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, GET, PATCH, DELETE, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+pub fn init_api(state: &Arc<AppState>) -> Result<()> {
     let state = Arc::clone(state);
 
+    let config = Config::build(Environment::Staging)
+        .address("0.0.0.0")
+        .port(45289)
+        // Without this there's a lot of spam when polling HA system state
+        .log_level(LoggingLevel::Critical)
+        .finalize()?;
+
     task::spawn(async move {
-        rocket::ignite()
+        rocket::custom(config)
+            .attach(CORS())
             .manage(state)
             .mount("/", routes![get_devices])
             .launch();
     });
+
+    Ok(())
 }
