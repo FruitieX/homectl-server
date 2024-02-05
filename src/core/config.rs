a219adb34e63b27fbbe918167c016d22aa4dd898
Rasmus Lövegren <rasmus@lovegren.fi>
2024-02-05 18:28:16 +0200

    fix: bypass config and use toml crate directly

--- src/core/config.rs
+++ src/core/config.rs
@@ -7,7 +7,7 @@ use crate::types::{
 use color_eyre::Result;
 use eyre::Context;
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File, io::Read};
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
@@ -30,16 +30,24 @@ pub fn read_config() -> Result<(Config, OpaqueIntegrationsConfigs)> {
     if !path.exists() && std::env::var("SKIP_SAMPLE_CONFIG").is_err() {
         error!("Settings.toml not found, generating sample configuration.");
         error!("Set SKIP_SAMPLE_CONFIG environment variable to opt out of this behavior.");
-        std::fs::copy(sample_path, path).unwrap();
+        std::fs::copy(sample_path, path.clone()).unwrap();
     }
 
     let builder = builder.add_source(config::File::with_name("Settings"));
 
     let settings = builder.build()?;
 
-    let config: Config = serde_path_to_error::deserialize(settings.clone()).wrap_err(
-        "Failed to deserialize config, compare your config file to Settings.toml.example!",
-    )?;
+    // TODO: until https://github.com/mehcode/config-rs/issues/531 is fixed
+    let config = {
+        let mut file = File::open(&path)?;
+        let mut contents = Default::default();
+        file.read_to_string(&mut contents)?;
+        toml::from_str(&contents)?
+    };
+
+    // let config: Config = serde_path_to_error::deserialize(settings.clone()).wrap_err(
+    //     "Failed to deserialize config, compare your config file to Settings.toml.example!",
+    // )?;
 
     let integrations_config = settings
         .get::<OpaqueIntegrationsConfigs>("integrations")
